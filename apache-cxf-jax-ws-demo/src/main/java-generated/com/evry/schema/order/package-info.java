89a0ade6b0fbcb38061edc837bd4e09c92803f8a
Shayan Yazdanmehr <shayan.yazdanmehr@evry.com>
2018-09-19 15:04:11 +0200

    Add Endpoint class

---
+++ apache-cxf-jax-ws-demo/src/main/java-generated/com/evry/schema/order/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.evry.com/schema/Order", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.evry.schema.order;
---
+++ apache-cxf-jax-ws-demo/src/main/java/com/evry/cxfdemo/orders/DefaultOrdersEndpoint.java
@@ -0,0 +1,38 @@
+package com.evry.cxfdemo.orders;
+
+import com.evry.schema.order.AccountType;
+import com.evry.schema.order.ObjectFactory;
+import com.evry.schema.order.OrderInquiryResponseType;
+import com.evry.schema.order.OrderInquiryType;
+import com.evry.service.orders.Orders;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.jws.WebService;
+
+@WebService(
+        portName = "OrderSOAP",
+        serviceName = "Orders",
+        endpointInterface = "com.evry.service.orders.Orders",
+        targetNamespace = "http://www.evry.com/service/Orders/"
+)
+public class DefaultOrdersEndpoint implements Orders {
+
+//    @Autowired
+//    private OrderService orderService;
+    @Override
+    public OrderInquiryResponseType processOrderPlacement(OrderInquiryType orderInquiry) {
+        ObjectFactory factory = new ObjectFactory();
+        OrderInquiryResponseType response = factory.createOrderInquiryResponseType();
+        AccountType account = factory.createAccountType();
+        account.setAccountId(1);
+        response.setAccount(account);
+        return response;
+//        OrderInquiryResponseType response = orderService.processOrder(
+//                orderInquiry.getUniqueOrderId(),
+//                orderInquiry.getOrderQuantity(),
+//                orderInquiry.getAccountId(),
+//                orderInquiry.getEan13()
+//        );
+//        return response;
+    }
+}
